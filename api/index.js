2767889639dcd914c8bbdc333b55db2f35b720fe
Mehul Warade <mehulwarade@gmail.com>
2020-06-15 16:18:59 +1000

    (CSV loading with API working)

---
+++ api/index.js
@@ -0,0 +1,19 @@
+/*
+  <=====  Initialisation for the node project ===========>
+*/
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8000
+
+app.listen(port, () => {
+  console.log(`API server port: ${port}`);
+});
+/*
+  <======  End initialisation ==========>
+*/
+
+const base = `${__dirname}/files`;
+
+app.get('/csv', function(req, res) {
+    res.sendFile(`${base}/Spray Log 20200608-111530.csv`);
+});
